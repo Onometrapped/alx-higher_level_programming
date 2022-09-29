7badd871fbb32d97482b5aa3ac7912bff078601a
Onometrapped <ewomazinookupa@gmail.com>
2022-09-28 17:55:30 -0700

    More data atructures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-
-def print_sorted_dictionary(a_dict):
-    """
-    print a dict in sorted order of keys
-    """
-    if a_dict is None:
-        return None
-    for key in sorted(a_dict.keys()):
-        print("{:s}: {}".format(key, a_dict[key]))
+# 7-update_dictionary.py
 
 
-if __name__ == '__main__':
-    a_dictionary = {
-        'language': "C", 'Number': 89,
-        'track': "Low level", 'ids': [1, 2, 3]
-    }
-    print_sorted_dictionary(a_dictionary)
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
